0e1384752cbc12779778fa98ddf004215c030036
Mohamed Bouhlel <mohamed.bouhleel@gmail.com>
2019-02-05 17:20:30 +0100

    yes

--- app/models/gossip.rb
+++ app/models/gossip.rb
@@ -1,6 +1,5 @@
 class Gossip < ApplicationRecord
-	validates title: , length: { minimum: 3, maximum: 14 }, presence: true #obligatoirement un title avec min 3 lettres et max 14
-	validates :content, presence: true #obligatoirement un content
+
 	belongs_to :user
 	has_many :comments
 	has_many :taggossips
@@ -8,3 +7,5 @@ class Gossip < ApplicationRecord
 	has_many :likes
 
 end
+
+
